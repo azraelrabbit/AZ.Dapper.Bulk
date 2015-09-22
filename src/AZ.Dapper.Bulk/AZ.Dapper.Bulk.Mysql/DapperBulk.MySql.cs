126dcb8a588d1535d03d4dfbc5ad86a487544a71
rocky <zhangjq@0plus.com.cn>
2015-09-22 18:54:34 +0800

    1

--- src/AZ.Dapper.Bulk/AZ.Dapper.Bulk.Mysql/DapperBulk.MySql.cs
+++ src/AZ.Dapper.Bulk/AZ.Dapper.Bulk.Mysql/DapperBulk.MySql.cs
@@ -12,95 +12,36 @@ namespace AZ.Dapper.Bulk
 {
     public static partial class DapperBulk
     {
-
-        private const string FieldTerminator = ",";
-        private const string LineTerminator = "\r\n";
-
         public static void BulkInsert(this MySqlConnection conn, DataTable sourceData)
         {
             var csvFile = string.Empty;
-            try
-            {
-                csvFile = CreateCsvFile(sourceData);
+            csvFile = CreateCsvFile(sourceData);
 
-                var bulkLoader = new MySqlBulkLoader(conn);
-                bulkLoader.TableName = sourceData.TableName;
-                bulkLoader.CharacterSet = "UTF-8";
-                bulkLoader.FieldTerminator = FieldTerminator;
-                bulkLoader.LineTerminator = LineTerminator;
-                bulkLoader.NumberOfLinesToSkip = 0;
-                bulkLoader.FileName = csvFile;
-                var ret = bulkLoader.Load();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                if (!string.IsNullOrEmpty(csvFile))
-                {
-                    if (File.Exists(csvFile))
-                    {
-                        try
-                        {
-                            File.Delete(csvFile);
-                        }
-                        catch
-                        {
-                        }
-                    }
-                }
-            }
+            DoBulkInert(conn, csvFile, sourceData.TableName);
         }
 
         public static void BulkInsert<T>(this MySqlConnection conn, List<T> sourceData, string destTableName)
         {
             var csvFile = string.Empty;
-            try
-            {
-                csvFile = CreateCsvFile(sourceData);
+            csvFile = CreateCsvFile(sourceData);
 
-                var bulkLoader = new MySqlBulkLoader(conn);
-                bulkLoader.TableName = destTableName;
-                bulkLoader.CharacterSet = "UTF-8";
-                bulkLoader.FieldTerminator = FieldTerminator;
-                bulkLoader.LineTerminator = LineTerminator;
-                bulkLoader.NumberOfLinesToSkip = 0;
-                bulkLoader.FileName = csvFile;
-                var ret = bulkLoader.Load();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                if (!string.IsNullOrEmpty(csvFile))
-                {
-                    if (File.Exists(csvFile))
-                    {
-                        try
-                        {
-                            File.Delete(csvFile);
-                        }
-                        catch
-                        {
-                        }
-                    }
-                }
-            }
+            DoBulkInert(conn, csvFile, destTableName);
         }
 
-        public static void BulkInsert<T>(this MySqlConnection conn, List<List<object>> sourceData, string destTableName)
+        public static void BulkInsert(this MySqlConnection conn, List<List<object>> sourceData, string destTableName)
         {
             var csvFile = string.Empty;
+            csvFile = CreateCsvFile(sourceData);
+
+            DoBulkInert(conn, csvFile, destTableName);
+        }
+
+        private static void DoBulkInert(MySqlConnection conn, string csvFile, string tablename)
+        {
             try
             {
-                csvFile = CreateCsvFile(sourceData);
-
                 var bulkLoader = new MySqlBulkLoader(conn);
-                bulkLoader.TableName = destTableName;
+                bulkLoader.TableName = tablename;
                 bulkLoader.CharacterSet = "UTF-8";
                 bulkLoader.FieldTerminator = FieldTerminator;
                 bulkLoader.LineTerminator = LineTerminator;
@@ -129,88 +70,5 @@ namespace AZ.Dapper.Bulk
                 }
             }
         }
-
-        static string CreateCsvFile(DataTable dataSource)
-        {
-            var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            var baseTempPath = Path.Combine(basePath, "App_Data");
-            var tempPath = Path.Combine(baseTempPath, "temp");
-
-            if (!Directory.Exists(tempPath))
-            {
-                Directory.CreateDirectory(tempPath);
-            }
-
-            var tmpFilePath = Path.Combine(tempPath, Guid.NewGuid().ToString("n") + ".csv");
-
-            foreach (DataRow row in dataSource.Rows)
-            {
-                var line = string.Join(FieldTerminator, row.ItemArray.ToArray());
-                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
-            }
-
-            return tmpFilePath;
-        }
-
-        static string CreateCsvFile<T>(List<T> dataSource)
-        {
-            var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            var baseTempPath = Path.Combine(basePath, "App_Data");
-            var tempPath = Path.Combine(baseTempPath, "temp");
-
-            if (!Directory.Exists(tempPath))
-            {
-                Directory.CreateDirectory(tempPath);
-            }
-
-            var tmpFilePath = Path.Combine(tempPath, Guid.NewGuid().ToString("n") + ".csv");
-
-            foreach (var item in dataSource)
-            {
-
-                var value = GetItemValue(item);
-                var line = string.Join(FieldTerminator, value);
-                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
-            }
-
-            return tmpFilePath;
-        }
-
-        static string CreateCsvFile (List<List<object>> dataSource)
-        {
-            var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            var baseTempPath = Path.Combine(basePath, "App_Data");
-            var tempPath = Path.Combine(baseTempPath, "temp");
-
-            if (!Directory.Exists(tempPath))
-            {
-                Directory.CreateDirectory(tempPath);
-            }
-
-            var tmpFilePath = Path.Combine(tempPath, Guid.NewGuid().ToString("n") + ".csv");
-
-            foreach (var item in dataSource)
-            {
-                var line = string.Join(FieldTerminator, item);
-                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
-            }
-
-            return tmpFilePath;
-        }
-
-        static List<object> GetItemValue<T>(T item)
-        {
-            var values=new List<object>();
-
-            var type = item.GetType();
-            var props=type.GetProperties();
-            foreach (var prop in props)
-            {
-                var ret=prop.GetValue(item, null);
-                values.Add(ret);
-            }
-
-            return values;
-        } 
     }
 }
--- src/AZ.Dapper.Bulk/AZ.Dapper.Bulk/Dapper.Bulk.cs
+++ src/AZ.Dapper.Bulk/AZ.Dapper.Bulk/Dapper.Bulk.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +9,77 @@ namespace AZ.Dapper.Bulk
 {
     public static partial  class DapperBulk
     {
+        private const string FieldTerminator = ",";
+        private const string LineTerminator = "\r\n";
 
+        static string CreateCsvFile(DataTable dataSource)
+        {
+            var tmpFilePath = GenerateTempFileName();
+
+            foreach (DataRow row in dataSource.Rows)
+            {
+                var line = string.Join(FieldTerminator, row.ItemArray.ToArray());
+                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
+            }
+
+            return tmpFilePath;
+        }
+
+        static string CreateCsvFile<T>(List<T> dataSource)
+        {
+            var tmpFilePath = GenerateTempFileName();
+
+            foreach (var item in dataSource)
+            {
+                var value = GetItemValue(item);
+                var line = string.Join(FieldTerminator, value);
+                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
+            }
+
+            return tmpFilePath;
+        }
+
+        static string CreateCsvFile(List<List<object>> dataSource)
+        {
+            var tmpFilePath = GenerateTempFileName();
+
+            foreach (var item in dataSource)
+            {
+                var line = string.Join(FieldTerminator, item);
+                File.AppendAllText(tmpFilePath, line + LineTerminator, Encoding.UTF8);
+            }
+
+            return tmpFilePath;
+        }
+
+        private static string GenerateTempFileName()
+        {
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            var baseTempPath = Path.Combine(basePath, "App_Data");
+            var tempPath = Path.Combine(baseTempPath, "temp");
+
+            if (!Directory.Exists(tempPath))
+            {
+                Directory.CreateDirectory(tempPath);
+            }
+
+            var tmpFilePath = Path.Combine(tempPath, Guid.NewGuid().ToString("n") + ".csv");
+            return tmpFilePath;
+        }
+
+        static List<object> GetItemValue<T>(T item)
+        {
+            var values = new List<object>();
+
+            var type = item.GetType();
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                var ret = prop.GetValue(item, null);
+                values.Add(ret);
+            }
+
+            return values;
+        }
     }
 }
